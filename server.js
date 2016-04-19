a617c28915a61f86633ed043a5aa86953d75d18f
Jesse Swart <jesse.swart@gmail.com>
2016-04-19 14:35:03 +0200

    Split off middleware

--- server.js
+++ server.js
@@ -2,17 +2,7 @@ var express = require('express');
 var app = express();
 var PORT = 3000
 
-var middleware = {
-	requireAuthentication: function(req, res, next){
-		console.log('private route hit');
-		next();
-	},
-	logger: function(req, res, next){
-		console.log('Request: ' + req.method+' '+req.originalUrl
-		 + ' on: ' + new Date().toString());
-		next();
-	}
-};
+var middleware = require('./middleware.js');
 
 app.use(middleware.logger);
 app.use(middleware.requireAuthentication);
